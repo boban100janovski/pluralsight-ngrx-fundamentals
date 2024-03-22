8f943225fea98074dc9d0fa5e109525cc7651ea0
Boban Stojanovski <boban.stojanovski@gmail.com>
2024-03-22 07:10:27 +0100

    init

---
+++ ngrx-fundamentals/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { environment } from 'src/environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { InMemoryDataService } from './in-memory-data.service';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      name: 'NgRx Demo App',
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ ngrx-fundamentals/src/app/products/state/product.reducer.ts
@@ -0,0 +1,19 @@
+import { createAction, createReducer, on } from '@ngrx/store';
+
+export interface ProductState {
+  showProductCode: boolean;
+}
+
+const initialState: ProductState = {
+  showProductCode: true,
+};
+
+export const productsReducer = createReducer(
+  initialState,
+  on(createAction('[Products Page] Toggle Show Product Code'), (state) => {
+    return {
+      ...state,
+      showProductCode: !state.showProductCode,
+    };
+  })
+);
