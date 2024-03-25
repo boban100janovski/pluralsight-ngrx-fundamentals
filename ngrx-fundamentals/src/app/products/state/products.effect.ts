a0914b3316873b39395cecf033881984db53103e
Boban Stojanovski <boban.stojanovski@gmail.com>
2024-03-25 08:20:48 +0100

    Update products.effect.ts with new CRUD operations

--- ngrx-fundamentals/src/app/products/state/products.effect.ts
+++ ngrx-fundamentals/src/app/products/state/products.effect.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, concatMap, map, of } from 'rxjs';
+import { catchError, concatMap, exhaustMap, map, mergeMap, of, switchMap } from 'rxjs';
 import { ProductsService } from '../products.service';
 import { ProductsAPIActions, ProductsPageActions } from './products.actions';
 
@@ -16,6 +16,42 @@ export class ProductEffects {
         this.productsService.getAll().pipe(
           map((products) => ProductsAPIActions.productsLoadedSuccess({ products })),
           catchError((error) => of(ProductsAPIActions.productsLoadedFail({ message: error })))
+        )
+      )
+    );
+  });
+
+  readonly addProduct$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(ProductsPageActions.addProduct),
+      exhaustMap(({ product }) =>
+        this.productsService.add(product).pipe(
+          map((product) => ProductsAPIActions.productAddedSuccess({ product })),
+          catchError((error) => of(ProductsAPIActions.productAddedFail({ message: error })))
+        )
+      )
+    );
+  });
+
+  readonly updateProduct$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(ProductsPageActions.updateProduct),
+      switchMap(({ product }) =>
+        this.productsService.update(product).pipe(
+          map((product) => ProductsAPIActions.productUpdatedSuccess({ product })),
+          catchError((error) => of(ProductsAPIActions.productAddedFail({ message: error })))
+        )
+      )
+    );
+  });
+
+  readonly deleteProduct$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(ProductsPageActions.deleteProduct),
+      mergeMap(({ id }) =>
+        this.productsService.delete(id).pipe(
+          map((product) => ProductsAPIActions.productDeletedSuccess({ id })),
+          catchError((error) => of(ProductsAPIActions.productDeletedFail({ message: error })))
         )
       )
     );
